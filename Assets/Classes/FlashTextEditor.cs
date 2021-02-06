fba6f0683ef64bfda3cf099c061f564eebe0e471
Jeff Minnear <jeffminnear@gmail.com>
2021-02-06 13:51:48 -0700

    Extend Unity Editor with FlashText class

---
+++ Assets/Classes/FlashTextEditor.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FlashText))]
+public class FlashTextEditor : UnityEditor.UI.TextEditor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        FlashText text = (FlashText)target;
+        text.flashable = EditorGUILayout.Toggle("Flashable", text.flashable);
+    }
+}
